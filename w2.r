e72af911b5e54277488de6f4f3843710d251e7e1
rafkruczkowski@github.com <kruczkowski@gmail.com>
2020-08-23 20:28:30 -0700

    need rework

--- w2.r
+++ w2.r
@@ -5,3 +5,33 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     sub5 <- sub1[!bad]
     mean(sub5)
 }
+pollutantmean("/mnt/c/Users/raf/CloudStation/Creative/DataSci/LearningR/rprog_data_specdata/specdata/ds_progr_w2", "sulfate", 1:10)
+/mnt/c/Users/raf/CloudStation/Creative/DataSci/LearningR/rprog_data_specdata/specdata/ds_progr_w2
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    file_list <- list.files(path=directory, pattern="*.csv")
+    dataset <- data.frame()
+    for (i in 1:length(file_list)) {
+        dataset <- read.csv(file_list[i])
+        sub1 <- subset(dataset, ID = id, select = pollutant)
+        bad <- is.na(sub1)
+        sub5 <- sub1[!bad]
+        result <- mean(sub5)
+        print(result)
+    }
+}
+
+complete <- function(directory, id = 1:332) {
+    file_list <- list.files(path=directory, pattern="*.csv")
+    dataset <- data.frame()
+    for (i in 1:length(file_list)) {
+        dataset <- read.csv(file_list[i])
+        sub1 <- subset(dataset, ID = id,)
+        sub2 <- nrow(sub1)
+        print(c(i, sub2))
+    }
+}
+
+
+
+list.files(path="/mnt/c/Users/raf/CloudStation/Creative/DataSci/LearningR/rprog_data_specdata/specdata/ds_progr_w2")
